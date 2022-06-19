00601199726d7cc2448541c77a520c78b9353504
tuncaytas <tnc.tas@gmail.com>
2022-06-19 22:46:42 +0000

    image cmodified

--- ecs/main.tf
+++ ecs/main.tf
@@ -124,7 +124,7 @@ resource "aws_ecs_task_definition" "main" {
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([{
     name        = "${var.name}-container-${var.environment}"
-    image       = "${var.container_image}:latest"
+    image       = "node:10"
     essential   = true
     environment = var.container_environment
     portMappings = [{
